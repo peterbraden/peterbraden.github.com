02063943b5552f3c7759b23b303623cd36c48a57
Peter Braden <PeterBraden@PeterBraden.co.uk>
2010-06-18 19:10:19 -0700

    isometric

--- isometric.js
+++ isometric.js
@@ -10,7 +10,8 @@ var get_coords = function(x, y, z, s){
 	return [(x-z) * 0.8944 *s, ((x+z) * 0.4472 - y)*s]
 }
 
-var drawHeightField = function(canv, plotf, heightf){
+
+var drawHeightField = function(canv, plotf, map){
 	var midx = CSIZE/2;
 	var midy = CSIZE/4;
 
@@ -21,28 +22,62 @@ var drawHeightField = function(canv, plotf, heightf){
 	ctx.fillStyle = "#222";
 	var tile_size = Math.sqrt(0.375) * CSIZE;
 	
+	var heightf = function(x,y){
+		var z = map[parseInt(x + y*MAP_SIZE)] || {}
+		return z.height
+		
+		}
+	
+	var indToCoord = function(i,j){
+		var c = get_coords(i, heightf(i,j), j, tile_size/MAP_SIZE);
+		var d = [inset + midx + c[0], top + c[1], heightf(i,j)];
+		if (d[0] && d[1]){
+			return d
+		} else {
+			return;
+		}
+	}
+	
+	
 	for (var i = 0; i<MAP_SIZE; i++){
 		for(var j = 0; j<MAP_SIZE; j++){
 			var c = get_coords(i, heightf(i,j), j, tile_size/MAP_SIZE);
-			plotf(i, j, [inset + midx + c[0], top + c[1]],  Math.sqrt(5) * ( tile_size/MAP_SIZE));
+			plotf(i, j, 
+				indToCoord(i,j),  
+				Math.sqrt(5) * ( tile_size/MAP_SIZE),
+				indToCoord
+				);
 		}
 	}
 
 }	
 
 
-
-
 var generateMap = function(){
-	map = new Array(MAP_SIZE*MAP_SIZE);
+	var map = new Array(MAP_SIZE*MAP_SIZE);
+	
+	var heightfield = $("#heightfield");
+	heightfield[0].width = MAP_SIZE;
+	heightfield[0].height = MAP_SIZE;
+	
+	//Have to create a frickin canvas
+	var hc = $("#hc");
+	hc[0].width = MAP_SIZE;
+	hc[0].height = MAP_SIZE;
+	var hcc = hc[0].getContext('2d');
+	hcc.drawImage(heightfield[0], 0, 0, MAP_SIZE, MAP_SIZE);
+
+	var hfdata = hcc.getImageData(0,0,MAP_SIZE, MAP_SIZE);
+	//console.log(hfdata)
 	for (var i = 0; i<map.length; i++){
 		var x = Math.random() > 0.95 ?
 			'tree' : Math.random() > 0.95 ?
 				Math.random() >0.5 ? 'rock' 
 				: 'tree2' 
 			: 'grass';
+		//console.log(hfdata.data[i*4+1]);	
 		map[i] = {
-			height:0,
+			height:hfdata.data[i*4+1]/25.5,
 			terrain:x,
 			open:true,
 		}
@@ -53,30 +88,83 @@ var generateMap = function(){
 }
 
 
+var imLoad = function(images, callback){
+	if (images.length){
+		var im = images.pop();
+		var img = new Image();
+		img.addEventListener('load', function(){imLoad(images, callback)}, false);
+		img.src = im;	
+	}
+	else{
+		callback();
+	}	
+}
 
 
 $(function(){
+	var imgs = [];
+	$('img').each(function(){
+		imgs.push(this.src);
+	});
 	
-	var canv = $("#isocanv")[0];
-	var ctx = canv.getContext('2d');
-	ctx.fillStyle = "#eee";
-	ctx.fillRect (0, 0, screen.width, screen.height);
+	imLoad(imgs, function(){
+		var canv = $("#isocanv")[0];
+		var ctx = canv.getContext('2d');
+		ctx.fillStyle = "#eee";
+		ctx.fillRect (0, 0, screen.width, screen.height);
+	
+		var MAP = generateMap();
+			
+		var point = function(x, y, c, s, toc){
+			//
+			// c n
+			// b o
+			
+			var n = toc(x+1, y)
+			var b = toc(x, y+1);
+			var o = toc(x+1, y+1);
+			
+			ctx.fillStyle = "#009900";
+			if (b){
+				var cb = plib.v3.normalise(plib.v3.sub(b, c));
+				var cn = plib.v3.normalise(plib.v3.sub(n, c));
+				var col = plib.v3.normalise(plib.v3.cross(cb, cn));
+				ctx.fillStyle = "rgb(0, " + parseInt(col[0]*2550 +100) + ",0)";
+			}
+			ctx.beginPath();
+			
+			
 
-	var MAP = generateMap();
+			ctx.moveTo(n[0], n[1]);
+			
+			
+			ctx.lineTo(c[0], c[1]);
+
+			if(b)
+				ctx.lineTo(b[0], b[1]);
+			
+			if(o)
+				ctx.lineTo(o[0], o[1]);
+			
+			
+			ctx.closePath();
+			ctx.fill();			
+			
+			
+			var m = $('#' + MAP[parseInt(x + y*MAP_SIZE)]['terrain'])[0];
+			ctx.drawImage(
+				m,
+				c[0]-s/2, c[1] - s* m.height/m.width,
+				s, s * m.height/m.width
+				);
+			//ctx.fillRect(c[0], c[1], 1, 1);	
+			
+		}
+	
+			
 		
-	var point = function(x, y, c, s){
-		var x = $('#' + MAP[parseInt(x + y*MAP_SIZE)]['terrain'])[0];		
-		ctx.drawImage(
-			x,
-			c[0]-s/2, c[1] - s* x.height/x.width,
-			s, s * x.height/x.width
-			);
-		//ctx.fillRect(c[0], c[1], 1, 1);	
-	}
-
-	var heightf = function(x, y){return MAP[parseInt(x + y*MAP_SIZE)]['height'];}	
-	
-	drawHeightField(canv, point, heightf);
+		drawHeightField(canv, point, MAP);
+	});
 });
 
 
